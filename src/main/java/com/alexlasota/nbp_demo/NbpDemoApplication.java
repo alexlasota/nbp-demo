2067ce7bfe0ce7133b8073391368db32de95b377
alexlasota <alexylasota@gmail.com>
2024-07-19 13:41:53 +0200

    hi

---
+++ src/main/java/com/alexlasota/nbp_demo/NbpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alexlasota.nbp_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbpDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexlasota/nbp_demo/NbpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexlasota.nbp_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
